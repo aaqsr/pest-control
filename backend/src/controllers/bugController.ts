9f3dc866e415397f2d4b76413d8f495fd9dca1cc
Awab <awab.qureshi@gmail.com>
2023-02-25 16:44:20 -0500

    Api changes with create bug

--- backend/src/controllers/bugController.ts
+++ backend/src/controllers/bugController.ts
@@ -5,22 +5,17 @@ import mongoose from "mongoose";
 // create a new Bug
 interface reqBody { 
     title: String,
-    load?: {
-        description?: String,
-        bug_level?: Number,
-    },
+    description?: String,
+    bug_level?: Number,
     // assigned_to: mongoose.Schema.Types.ObjectId
     assigned_to?: String
 }
 
 export const createBug = async (req: express.Request, res: express.Response) => {
-    const { title, load, assigned_to }: reqBody = req.body;
-    
-    const desc = load?.description;
-    const bug_lvl = load?.bug_level;
+    const { title, description, bug_level, assigned_to }: reqBody = req.body;
 
     try {
-        const bug_doc = await Bug.create({title, desc, bug_lvl, assigned_to});
+        const bug_doc = await Bug.create({title, description, bug_level, assigned_to});
         res.status(200).json(bug_doc);
     } catch (error) {
         res.status(400).json({error: error}); // u messed up
