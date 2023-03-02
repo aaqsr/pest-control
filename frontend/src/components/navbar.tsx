18826cc0e56d35853728d4a46f4aa49ae61f1a13
Awab <awab.qureshi@gmail.com>
2023-03-02 01:36:41 -0500

    Updated navbar TODO: hide login/signup when logged in

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,22 +1,31 @@
 import { Link } from 'react-router-dom'
+import { useLogOut } from '../hooks/useLogOut'
 
 const Navbar = () => {
+    const { logout } = useLogOut();
 
-  return (
-    <header>
-      <div className="container">
-        <Link to="/">
-          <h1>Pest Control</h1>
-        </Link>
-        <nav>
-          <div>
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Signup</Link>
-          </div>
-        </nav>
-      </div>
-    </header>
-  )
+    const handleLogOutButtonClick = () => {
+        logout();
+    }
+
+    return (
+        <header>
+            <div className="container">
+                <Link to="/">
+                    <h1>Pest Control</h1>
+                </Link>
+                <nav>
+                    <div>
+                        <button onClick={handleLogOutButtonClick}>Log out</button>
+                    </div>
+                    <div>
+                        <Link to="/login">Login</Link>
+                        <Link to="/signup">Signup</Link>
+                    </div>
+                </nav>
+            </div>
+        </header>
+    )
 }
 
 export default Navbar
