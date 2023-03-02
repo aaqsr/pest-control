a5dc0b36bd18351d55d6585faf8c014f545252e5
Awab <awab.qureshi@gmail.com>
2023-03-02 01:11:26 -0500

    Added useSignUp

---
+++ frontend/src/hooks/useSignUp.ts
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { useAuthContext } from "./useAuthContext";
+import { APP_URL } from "../global";
+
+export const useSignUp = () => {
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading]: [null|boolean, any] = useState(null);
+    const { dispatch }: any = useAuthContext();
+
+    const signup = async (email: string, password: string) => {
+        setIsLoading(true);
+        setError(null);
+
+        const response = await fetch(APP_URL + '/api/user/signup', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email, password })
+        });
+
+        const json = await response.json();
+
+        if (!response.ok) {
+            setIsLoading(false);
+            setError(json.error);
+        }
+        
+        if (response.ok) {
+            // save user to local storage
+            localStorage.setItem('user', JSON.stringify(json));
+
+            // update authContext
+            dispatch({ type: 'LOGIN', payload: json });
+
+            setIsLoading(false);
+        }
+    }
+
+    return { signup, isLoading, error };
+}
