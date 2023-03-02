4d545da585d59e58cd4c378cce271eab675de852
Awab <awab.qureshi@gmail.com>
2023-03-02 00:17:20 -0500

    Added auth context

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { AuthContextProvider } from './context/authContext'
 import { BugsContextProvider } from './context/bugContext'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BugsContextProvider>
-      <App />
-    </BugsContextProvider>
+    <AuthContextProvider>
+      <BugsContextProvider>
+        <App />
+      </BugsContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>,
 )
