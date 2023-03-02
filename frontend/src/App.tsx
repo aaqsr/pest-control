b77683fe7b3ac8c7dc9dfda7f5cfa21e50d8df3c
Awab <awab.qureshi@gmail.com>
2023-03-02 01:35:15 -0500

    Formatting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,29 +5,29 @@ import Login from "./pages/Login"
 import SignUp from "./pages/SignUp"
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <div className="pages">
-          <Routes>
-            <Route
-              path="/"
-              element={<Home />}
-            />
-            <Route
-              path="/login"
-              element={<Login />}
-            />
-            <Route
-              path="/signup"
-              element={<SignUp />}
-            />
-          </Routes>
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Navbar />
+                <div className="pages">
+                    <Routes>
+                        <Route
+                            path="/"
+                            element={<Home />}
+                        />
+                        <Route
+                            path="/login"
+                            element={<Login />}
+                        />
+                        <Route
+                            path="/signup"
+                            element={<SignUp />}
+                        />
+                    </Routes>
+                </div>
+            </BrowserRouter>
         </div>
-      </BrowserRouter>
-    </div>
-  )
+    )
 }
 
 export default App
