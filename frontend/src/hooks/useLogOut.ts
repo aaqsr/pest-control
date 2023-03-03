13f0ae810519c8fc10b331a340b91c7c86eedd6c
Awab <awab.qureshi@gmail.com>
2023-03-03 18:43:31 -0500

    Added bugs to users to frontend

--- frontend/src/hooks/useLogOut.ts
+++ frontend/src/hooks/useLogOut.ts
@@ -1,14 +1,17 @@
 import { useAuthContext } from "./useAuthContext";
+import { useBugsContext } from "./useBugsContext";
 
 export const useLogOut = () => {
-    const { dispatch }: any = useAuthContext();
+    const { dispatch: authDispatch }: any = useAuthContext();
+    const { dispatch: bugsDispatch } = useBugsContext();
 
     const logout = () => {
         // remove user from storage
         localStorage.removeItem('user');
 
         // dispatch logout action
-        dispatch({ type: 'LOGOUT' });
+        authDispatch({ type: 'LOGOUT' });
+        bugsDispatch({ type: 'SET_BUGS', payload: null });
     }
 
     return { logout };
