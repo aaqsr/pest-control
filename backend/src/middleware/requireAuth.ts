202a4b6c25d65a98953e294ac388f94845fbb632
Awab <awab.qureshi@gmail.com>
2023-03-03 15:11:19 -0500

    Fixed env var bug

--- backend/src/middleware/requireAuth.ts
+++ backend/src/middleware/requireAuth.ts
@@ -2,7 +2,7 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import user from "../models/user";
 
-export const requireAuth = async (req: any, res: express.Response, next:Function) => {
+export const requireAuth = async (req: any, res: express.Response, next: Function) => {
 
     // verify authentication
     const { authorization } = req.headers;
@@ -11,17 +11,17 @@ export const requireAuth = async (req: any, res: express.Response, next:Function
         res.status(401).json({ error: "Authorization Token Required" });
     }
 
-    const token = authorization?.split(' ')[1];
+    const token = authorization.split(' ')[1];
 
     if (token) {
         try {
-            if (process.env.SECRET) {
-                const sec: jwt.Secret = process.env.SECRET;
+            if (process.env.JWT_SECRET_KEY) {
+                const sec: jwt.Secret = process.env.JWT_SECRET_KEY;
                 const { _id }: any = jwt.verify(token, sec);
                 req.user = await user.findById(_id);
 
                 next();
-            }
+            } else { res.status(500).json({ error: "No jwt secret found in env." }) }
         } catch (error) {
             console.error(error);
             res.status(401).json({ error: "Request is not authorised" });
