ea32a0c7d947c18a27cd8368245b0bd9cdc7b6e8
Awab <awab.qureshi@gmail.com>
2023-03-03 02:38:42 -0500

    Fixed getBug route

--- backend/src/routes/bugs.ts
+++ backend/src/routes/bugs.ts
@@ -1,13 +1,11 @@
 import express from "express";
-import { createBug, delBug, getBugs, updateBug } from "../controllers/bugController";
+import { createBug, delBug, getBug, getBugs, updateBug } from "../controllers/bugController";
 
 export const router = express.Router();
 
 router.get('/', getBugs);
 
-router.get('/:id', (req, res) => {
-    res.json({msg: "GET one bug"});
-});
+router.get('/:id', getBug);
 
 // Post a bug
 router.post('/', createBug)
